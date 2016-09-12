634c629dd35a93546e9cdff88cd620512482c6c4
Leonard Lee <sheeeng@gmail.com>
2016-09-12 22:25:25 +0200

    Add reverse string function.

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 exports.printExampleMessage = function() {
   console.log("This is an example message from the example package.");
 }
+
+exports.reverseString(s) {
+  return s.split('').reverse().join('');
+}
---
+++ test.js
@@ -0,0 +1,7 @@
+const stringreversal = require('string-reversal-example')
+
+stringreversal.printExampleMessage();
+
+var testString1 = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.';
+
+console.log(reverseString(testString1));
