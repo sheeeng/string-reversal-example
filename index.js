bf1b5639734e63bbcaa1ee71a8c4780c0a1cc99a
Leonard Lee <sheeeng@gmail.com>
2016-09-12 23:35:45 +0200

    Add tests.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-exports.printExampleMessage = function () {
-  console.log('This is an example message from the example package.')
+exports.showExampleMessage = function () {
+  return 'This is an example message from the example package.'
 }
 
 var reverseString = function (s) {
--- test.js
+++ test.js
@@ -1,7 +1,27 @@
-const stringreversal = require('string-reversal-example')
+var assert = require('assert')
+describe('Array', function () {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal(-1, [1, 2, 3].indexOf(4))
+    })
+  })
+})
 
-stringreversal.printExampleMessage()
+describe('Message', function () {
+  describe('#showExampleMessage()', function () {
+    it('should show example message', function () {
+      var stringreversal = require('./index.js')
+      assert.equal('This is an example message from the example package.', stringreversal.showExampleMessage())
+    })
+  })
+})
 
-var testString1 = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-
-console.log(stringreversal.reverseString(testString1))
+describe('Reverse', function () {
+  describe('#reverseString()', function () {
+    it('should reverse the string', function () {
+      var stringreversal = require('./index.js')
+      var testString1 = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+      assert.equal(stringreversal.reverseString(testString1), '.tile gnicsipida rutetcesnoc ,tema tis rolod muspi meroL')
+    })
+  })
+})
