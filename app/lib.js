296019fbf41fa682faf50c8bca21d25b9a8c48f7
Leonard Lee <sheeeng@gmail.com>
2016-09-13 18:45:45 +0200

    Tidy up directories.

---
+++ app/lib.js
@@ -0,0 +1,8 @@
+exports.getExampleMessage = function () {
+  return 'This is an example message from the example package.'
+}
+
+var reverseString = function (s) {
+  return s.split('').reverse().join('')
+}
+module.exports.reverseString = reverseString
--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-exports.getExampleMessage = function () {
-  return 'This is an example message from the example package.'
-}
-
-var reverseString = function (s) {
-  return s.split('').reverse().join('')
-}
-module.exports.reverseString = reverseString
+require('./app/lib')
